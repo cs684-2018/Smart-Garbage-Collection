532fa7779cb8d5cc3da397415ce426091304b55d
manasdas17 <manasdas17@gmail.com>
2018-04-29 23:17:39 +0530

    final submission

---
+++ 2018-cs684-Smart-Garbage-Collection/Lab Experiments/163236001/lab7/Debug/main.d
@@ -0,0 +1,45 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.3.LTS/include/stdint.h
+main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.3.LTS/include/stdbool.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.2.111/inc/hw_types.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.2.111/inc/hw_memmap.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/sysctl.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/gpio.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.2.111/inc/tm4c123gh6pm.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/debug.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/pin_map.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.2.111/inc/hw_gpio.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/adc.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/rom.h
+main.obj: ../one.h
+main.obj: ../two.h
+main.obj: ../three.h
+main.obj: ../four.h
+main.obj: ../five.h
+main.obj: ../six.h
+main.obj: ../seven.h
+main.obj: ../eight.h
+
+../main.c: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.3.LTS/include/stdint.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.3.LTS/include/stdbool.h: 
+C:/ti/TivaWare_C_Series-2.1.2.111/inc/hw_types.h: 
+C:/ti/TivaWare_C_Series-2.1.2.111/inc/hw_memmap.h: 
+C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/sysctl.h: 
+C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/gpio.h: 
+C:/ti/TivaWare_C_Series-2.1.2.111/inc/tm4c123gh6pm.h: 
+C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/debug.h: 
+C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/pin_map.h: 
+C:/ti/TivaWare_C_Series-2.1.2.111/inc/hw_gpio.h: 
+C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/adc.h: 
+C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/rom.h: 
+../one.h: 
+../two.h: 
+../three.h: 
+../four.h: 
+../five.h: 
+../six.h: 
+../seven.h: 
+../eight.h: 
--- README.md
+++
@@ -1,1 +0,0 @@
-# Smart-Garbage-Collection
